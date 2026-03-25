d48a9ff433d069975985b040841b5a60ad49ed5c
TimofeyMeshcheryakov <tdmesherykov@mail.ru>
2026-03-25 15:10:41 +0300

    save

---
+++ app/src/main/java/com/totmyanin/lab14_shuvalova_isp_231/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.totmyanin.lab14_shuvalova_isp_231
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.totmyanin.lab14_shuvalova_isp_231.ui.theme.Lab14_shuvalova_isp231Theme
+import com.totmyanin.lab14_shuvalova_isp_231.ui_model.GameScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab14_shuvalova_isp231Theme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    innerPadding ->
+                    GameScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab14_shuvalova_isp231Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/totmyanin/lab14_shuvalova_isp_231/ui_model/GameViewModel.kt
@@ -0,0 +1,91 @@
+package com.totmyanin.lab14_shuvalova_isp_231.ui_model
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import com.totmyanin.lab14_shuvalova_isp_231.data.GameUiState
+import com.totmyanin.lab14_shuvalova_isp_231.data.allWords
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import com.totmyanin.lab14_shuvalova_isp_231.data.MAX_NO_OF_WORDS
+import com.totmyanin.lab14_shuvalova_isp_231.data.SCORE_INCREASE
+import kotlinx.coroutines.flow.update
+
+class GameViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+    var userGuess by mutableStateOf("")
+        private set
+
+    init {
+        resetGame()
+    }
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(
+            currentScrambledWord = pickRandomWordAndShuffle()
+        )
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord) == word) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        while (usedWords.contains(currentWord)) {
+            currentWord = allWords.random()
+        }
+        usedWords.add(currentWord)
+        return shuffleCurrentWord(currentWord)
+    }
+    fun updateUserGuess(guessedWord: String) {
+        userGuess = guessedWord
+    }
+
+    private fun updateGameStae(updatedScore: Int) {
+        if (usedWords.size == MAX_NO_OF_WORDS) {
+            _uiState.update {
+                currentState -> currentState.copy (
+                    isGuessedWordWrong = false,
+                    score = updatedScore,
+                    isGameOver = true
+                )
+            }
+        }
+        else
+        {
+            _uiState.update {
+                currentState -> currentState.copy(
+                    isGuessedWordWrong = false,
+                    currentScrambledWord = pickRandomWordAndShuffle(),
+                    score = updatedScore,
+                    currentWordCount = currentState.currentWordCount + 1
+                )
+            }
+        }
+    }
+
+    fun checkUserGuess() {
+        if (userGuess.equals(currentWord, ignoreCase = true)) {
+            val updatedScore = _uiState.value.score + SCORE_INCREASE
+            updateGameStae(updatedScore)
+        } else {
+            _uiState.update { currentState -> currentState.copy(isGuessedWordWrong = true) }
+        }
+        updateUserGuess("")
+    }
+
+    fun skipWord() {
+        updateGameStae(_uiState.value.score)
+        updateUserGuess("")
+    }
+}
